bf7856e949686aa252567c3b57c7d3dad02500ac
Max Buslaev <max@buslaev.net>
2024-12-04 21:10:37 +0300

    toplevel comment

--- lib/json_data_extractor.rb
+++ lib/json_data_extractor.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 require 'jsonpath'
 require_relative 'json_data_extractor/version'
 require_relative 'json_data_extractor/configuration'
 require_relative 'json_data_extractor/extractor'
 require_relative 'json_data_extractor/schema_element'
 
+# Transform JSON data structures with the help of a simple schema and JsonPath expressions.
+# Use the JsonDataExtractor gem to extract and modify data from complex JSON structures using a straightforward syntax
+# and a range of built-in or custom modifiers.
 module JsonDataExtractor
   class << self
     # Backward compatibility
