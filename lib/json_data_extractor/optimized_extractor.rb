500770456b3feaa9d223290732cc8d66eff01b8a
Max Buslaev <max@buslaev.net>
2025-11-26 07:55:27 +0300

    replace transform_values for older rubies

--- lib/json_data_extractor/optimized_extractor.rb
+++ lib/json_data_extractor/optimized_extractor.rb
@@ -121,7 +121,7 @@ module JsonDataExtractor
     def deep_dup(obj)
       case obj
       when Hash
-        obj.transform_values { |v| deep_dup(v) }
+        obj.each_with_object({}) { |(k, v), h| h[k] = deep_dup(v) }
       when Array
         obj.map { |v| deep_dup(v) }
       else
@@ -131,7 +131,7 @@ module JsonDataExtractor
   end
 end
 
-# Ruby basic types helper
+    # Ruby basic types helper
 class Object
   def duplicable?
     true
